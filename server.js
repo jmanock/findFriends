f903415d2c7c699422620aa64a8fc20f6b2bde59
Jon Manock <jonmanock@gmail.com>
2017-03-24 18:31:57 -0400

    think i might be on a track

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var url = 'http://flvoters.com/by_zip.html';
 var addons = 'http://flvoters.com/by_address/';
-
+var http = require('http');
 
 request(url, function(error, response, body){
   if(!error && response.statusCode === 200){
@@ -18,6 +18,13 @@ function First(address){
   if(address === 'HOME PAGE'){
 
   }else{
-    console.log(address);
+    http.createServer(function(req,res){
+      res.writeHead(200, {
+        'Content-Type':'text/html'
+      });
+      res.write('<h1>'+address+'</h1>');
+      res.end();
+    }).listen(3000);
+    console.log('HelloFriend lets see if this works at 3k');
   }
 }
