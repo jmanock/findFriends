86eac127085697de3b6aa72a5e8f27cab9f72d8a
Jon Manock <jonmanock@gmail.com>
2017-03-24 20:13:41 -0400

    basic works

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.render('pages/index');
+  var drinks = [
+    {name:'Blood Mary', drunkness:3},
+    {name:'Martini', drunkness:5},
+    {name:'Scotch', drunkness:10}
+  ];
+  var tagline = 'Any code of your own that you have not looked at for six or more months might as well have been written by someone else';
+  res.render('pages/index',{
+    drinks:drinks,
+    tagline:tagline
+  });
 });
 
 app.get('/about', function(req, res){
