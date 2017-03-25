0f2e1fa4f5f0ac9e21193b48e00784c6ed057bbb
Jon Manock <jonmanock@gmail.com>
2017-03-24 21:51:59 -0400

    something good is here just cant figure it out

---
+++ save/server.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var url = 'http://flvoters.com/by_zip.html';
+var addons = 'http://flvoters.com/by_address/';
+
+
+request(url, function(error, response, body){
+  if(!error && response.statusCode === 200){
+    var $ = cheerio.load(body);
+    $('a').each(function(i, data){
+        var address = $(this).text();
+        First(address);
+    });
+  }
+});
+function First(address){
+  if(address === 'HOME PAGE'){
+
+  }else{
+    console.log(address);
+  }
+}
